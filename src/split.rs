35b2c0a12a0ddb0ccbfc82787bc37918874349bd
Toru Ogawa <Hakuyume@users.noreply.github.com>
2021-04-11 17:03:18 +0900

    add tests

--- src/split.rs
+++ src/split.rs
@@ -8,6 +8,7 @@ use std::ops::RangeInclusive;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
+#[derive(Debug, PartialEq)]
 pub struct Part {
     pub body: Vec<Bytes>,
     pub content_length: usize,
@@ -83,9 +84,11 @@ impl Inner {
     }
 
     fn push_part(&mut self, chunk: Bytes) {
-        self.part_content_length += chunk.len();
-        self.part_content_md5.update(&chunk);
-        self.part_body.push(chunk);
+        if !chunk.is_empty() {
+            self.part_content_length += chunk.len();
+            self.part_content_md5.update(&chunk);
+            self.part_body.push(chunk);
+        }
     }
 
     fn push(&mut self, chunk: Bytes) {
@@ -122,5 +125,69 @@ impl Inner {
             content_md5: self.part_content_md5.finalize(),
             part_number: self.part_number + 1,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{split, Part};
+    use bytes::Bytes;
+    use futures::StreamExt;
+    use md5::{Digest, Md5};
+
+    #[tokio::test]
+    async fn test_split() {
+        let mut parts = split::<_, ()>(
+            futures::stream::iter(
+                vec![
+                    &[0, 1, 2][..],
+                    &[3, 4],
+                    &[
+                        5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                    ],
+                    &[22, 23],
+                ]
+                .into_iter()
+                .map(|chunk| Ok(Bytes::from_static(chunk))),
+            ),
+            4..=8,
+        );
+        assert_eq!(
+            parts.next().await,
+            Some(Ok(Part {
+                body: vec![Bytes::from_static(&[0, 1, 2]), Bytes::from_static(&[3, 4])],
+                content_length: 5,
+                content_md5: Md5::digest(&[0, 1, 2, 3, 4]),
+                part_number: 1,
+            }))
+        );
+        assert_eq!(
+            parts.next().await,
+            Some(Ok(Part {
+                body: vec![Bytes::from_static(&[5, 6, 7, 8, 9, 10, 11, 12])],
+                content_length: 8,
+                content_md5: Md5::digest(&[5, 6, 7, 8, 9, 10, 11, 12]),
+                part_number: 2,
+            }))
+        );
+        assert_eq!(
+            parts.next().await,
+            Some(Ok(Part {
+                body: vec![Bytes::from_static(&[13, 14, 15, 16, 17, 18, 19, 20])],
+                content_length: 8,
+                content_md5: Md5::digest(&[13, 14, 15, 16, 17, 18, 19, 20]),
+                part_number: 3,
+            }))
+        );
+        assert_eq!(
+            parts.next().await,
+            Some(Ok(Part {
+                body: vec![Bytes::from_static(&[21]), Bytes::from_static(&[22, 23])],
+                content_length: 3,
+                content_md5: Md5::digest(&[21, 22, 23]),
+                part_number: 4,
+            }))
+        );
+        assert_eq!(parts.next().await, None);
     }
 }
